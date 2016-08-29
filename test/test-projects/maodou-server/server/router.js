47f776fab8f57875583d3cb4b8cd1c46ed3e987d
BeijiYang <langyuyang@outlook.com>
2016-08-29 16:03:43 +0800

    maodou-cli login

--- test/test-projects/maodou-server/server/router.js
+++ test/test-projects/maodou-server/server/router.js
@@ -14,8 +14,17 @@ var post = Picker.filter(function(req, res) {
 //    curl -X POST --data "message=MyFirstMessage" http://localhost:3000/feed
 post.route('/user/authenticate', function(params, req, res, next) {
   console.log("req data is:", req.body);
+  //console.log(process.env);
+  console.log("The user is:", req.body.login);
+  console.log("The password is:", req.body.password);
 
-  var ret = {"id":"49372","company":"Maodou Inc.","created_datetime":"Thu, 06 Aug 2015 21:34:41 GMT","email":"limingth@maodou.io","firstName":"","flags":{"isVerified":false},"jobTitle":"CEO","lastName":"","level":"NORMAL","projectLimit":"50","servoLimit":"10","status":"NORMAL","username":"limingth","isOrg":false,"linkedAccounts":{},"authToken":"ef5a625e-7bd5-45e8-9ffc-26798c373021"};
+  // if(!req.body.login || !req.body.password){
+  //   res.end(JSON.stringify({"1024"}));
+  // };
 
-  res.end(JSON.stringify(ret));
+  var ret = {"id":"49372","company":"Maodou Inc.","created_datetime":"Thu, 06 Aug 2015 21:34:41 GMT","email":"limingth@maodou.io","firstName":"","flags":{"isVerified":false},"jobTitle":"CEO","lastName":"","level":"NORMAL","projectLimit":"50","servoLimit":"10","status":"NORMAL","username":req.body.login,"isOrg":false,"linkedAccounts":{},"authToken":"ef5a625e-7bd5-45e8-9ffc-26798c373021"};
+  if(req.body.login){
+    res.end(JSON.stringify(ret));
+  }
+
 });
